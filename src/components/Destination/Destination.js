c0af7b2bbde00c8f5e166a520e863b5bf949fa80
khalid <mdmahfuzbd@gmail.com>
2021-03-21 19:14:20 +0600

    search-result updated

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -5,33 +5,43 @@ import './Destination.css';
 import map from '../../images/Transport/map.png';
 import Ride from '../Ride/Ride';
 import rideData from '../fakeData/transport.json';
-import { useParams } from 'react-router-dom';
 import { RideContext } from '../../App';
 
 const Destination = (props) => {
-    const [rideType,setRideType] = useContext(RideContext);
-    // const rideType = props.type;
-    // console.log('Ride-type:',rideType);
-    // const {type} = useParams();
-    // console.log('hhh',type)
+
+    const [rideType] = useContext(RideContext);
+
     let type = rideType || 'car';
-    console.log('ride=type:',rideType)
+    console.log('ridetypefromDestination:', rideType)
+
+    const [location, setLocation] = useState({
+        from: '',
+        to: '',
+    });
 
     const [rides, setRides] = useState([]);
     useEffect(() => { setRides(rideData) }, []);
 
-
-    const [location, setLocation] = useState({
-        from: '',
-        to: 'd',
-    });
+    const searchAgain = () => {
+        setLocation('');
+    }
 
 
-    const handleSearch = () => {
+    const handleBlur = (evnt) => {
+        console.log(evnt.target.name, evnt.target.value)
+        const newLocation = { ...location }; //copy object
+        newLocation[evnt.target.name] = evnt.target.value; //set object property
+        setLocation(newLocation)
+    }
+    console.log(location)
+
+    const handleSearch = (e) => {
+        //todo for google maps
+        e.preventDefault();
         console.log('search clicked');
     }
     return (
-        <div>           
+        <div>
             <div className='destination-container'>
 
                 <Container>
@@ -40,24 +50,37 @@ const Destination = (props) => {
 
                             <div className='desti-form-container'>
                                 <p> from-to</p>
-                                <form onSubmit={handleSearch} className='desti-form'>
-                                    <div className='desti-form-inside'>
+                                {!location.to &&
+                                    <form onSubmit={handleSearch} className='desti-form'>
+                                        <div className='desti-form-inside'>
 
-                                        <label >Pick From</label>
-                                        <input type='text' name="origin" placeholder='from' required />
-                                        <label >Pick To</label>
-                                        <input type='text' name="destination" placeholder='Destination' required />
-                                        <input type="submit" value='Search' style={{ marginTop: '1rem' }} />
+                                            <label >Pick From</label>
+                                            <input type='text' name="from" placeholder='from' required onBlur={handleBlur} />
+                                            <label >Pick To</label>
+                                            <input type='text' name="to" placeholder='Destination' required onBlur={handleBlur} />
+                                            <input type="submit" value='Search' style={{ marginTop: '1rem' }} />
 
-                                    </div>
-                                </form>
+                                        </div>
+                                    </form>
+                                }
                                 {location.to &&
                                     <div className='search-result'>
-                                        <h1>Search result</h1>
-                                        {
-                                            rides.filter((ride) =>ride.type===type).map((ride)=><Ride key={ride.id} ride={ride}></Ride>)
-                                        }
-                                        
+                                        <div className='search-location'>
+                                            <p id='search-again' onClick={searchAgain}>Search Again</p>
+                                            <div id='search-from-to'>
+                                                <p>From: {location.from}</p>
+                                                <p>To: {location.to}</p>
+                                            </div>
+
+
+                                        </div>
+                                        <div className='search-ride'>
+                                            {
+                                                rides.filter((ride) => ride.type === type).map((ride) => <Ride key={ride.id} ride={ride}></Ride>)
+                                            }
+                                        </div>
+
+
                                     </div>
                                 }
                             </div>
@@ -76,7 +99,7 @@ const Destination = (props) => {
             </div>
 
 
-         
+
         </div>
     );
 };
